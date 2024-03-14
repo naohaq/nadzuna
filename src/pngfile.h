20a55476c86a50a953fd1c16f211763a1def0f35
Naoyuki MORITA <naohaq@ps.sakura.ne.jp>
2024-03-14 10:56:41 +0900

    Implement load_png().

---
+++ src/pngfile.h
@@ -0,0 +1,27 @@
+/* -*- mode: c; coding: utf-8-unix -*- */
+/*
+ * pngfile.h
+ *
+ *  by Naoyuki MORITA <naohaq@ps.sakura.ne.jp>
+ */
+
+#ifndef NADZUNA_PNGFILE_H_
+#define NADZUNA_PNGFILE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern BitmapImage_t * load_png(const char * filename);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* NADZUNA_PNGFILE_H_ */
+
+/*
+ * Local Variables:
+ * indent-tabs-mode: t
+ * tab-width: 4
+ * End:
+ */
